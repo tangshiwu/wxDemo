1d947d933846f8569141292a5ab758dec598b987
tangshiwu <1726213442@qq.com>
2021-12-17 00:36:19 +0800

    登录

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,17 +1,37 @@
 // pages/user/user.js
+let util = require('../../utils/util.js') // 引入公共代码
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userContenr:[
-      {type: 'tickets', name: '电影票'},
-      {type: 'coupons', name: '优惠券'},
-      {type: 'equityCards', name: '权益卡'},
-      {type: 'snack', name: '我的小食'},
-      {type: 'wantToSee', name: '想看的电影'},
-      {type: 'seen', name: '看过的电影'},
+    avatarUrl: null,
+    nickName: null,
+    userContenr: [{
+        type: 'tickets',
+        name: '电影票'
+      },
+      {
+        type: 'coupons',
+        name: '优惠券'
+      },
+      {
+        type: 'equityCards',
+        name: '权益卡'
+      },
+      {
+        type: 'snack',
+        name: '我的小食'
+      },
+      {
+        type: 'wantToSee',
+        name: '想看的电影'
+      },
+      {
+        type: 'seen',
+        name: '看过的电影'
+      },
     ]
   },
 
@@ -19,12 +39,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   login() {
-    wx.login({
-      success: function(res) {
-        console.log(res);
-      }
+    util.userLogin((res) => {
+      console.log("回调：", res)
+      this.setData({
+        avatarUrl: res.avatarUrl,
+        nickName: res.nickName,
+      })
     })
   },
+
+  onLoad: function (options) {
+    let userInfo = wx.getStorageSync('userInfo')
+    if (userInfo) {
+      this.setData({
+        avatarUrl: userInfo.avatarUrl,
+        nickName: userInfo.nickName,
+      })
+    }
+  },
+
   /**
    * 页面上拉触底事件的处理函数
    */
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,108 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+const userLogin = (successFn) => {
+  wx.showLoading({
+    title: '登录中',
+  })
+  let endData = {} // 成功回调暴露出去的结果
+  if (wx.getUserProfile) {
+    let loginCode = ''
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          loginCode = res.code
+        } else {
+          wx.showToast({
+            title: '登录失败',
+            icon: 'error'
+          })
+          // console.log("调用wx.login获取code失败")
+        }
+      },
+      fail: function (error) {
+        wx.showToast({
+          title: '登录失败,请重试',
+          icon: 'error'
+        })
+        // console.log("wx.login接口调用失败", error)
+      },
+    })
+    wx.getUserProfile({
+      desc: '用于获取头像、昵称等信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (infoRes) => {
+        console.log("登录结果：", infoRes.userInfo)
+        endData = {
+          avatarUrl: infoRes.userInfo.avatarUrl,
+          nickName: infoRes.userInfo.nickName,
+        }
+        saveUserInfo(infoRes.userInfo)
+        successFn(endData)
+        // wx.request({
+        //   url: `${getApp().globalData.baseUrl}/login/wechat`,
+        //   method: "POST",
+        //   data: {
+        //     code: loginCode,
+        //     rawData: infoRes.rawData, // 用户非敏感信息
+        //     signature: infoRes.signature, //签名
+        //     encryptedData: infoRes.encryptedData, // 用户敏感信息
+        //     iv: infoRes.iv //解密算法的向量
+        //     // token: wx.getStorageSync("loginFlag")
+        //   },
+        //   success: function (res) {
+        //     // console.log("login success", res)
+        //     res = res.data;
+        //     if (res.code == 200) {
+        //       endData = {
+        //         userInfo: res.data.user,
+        //       }
+        //       // 设置本地存储(下次登录如果本次存储存在则无需再次登录)
+        //       getApp().saveUserInfo(res.data.user)
+        //       successFn(endData)
+        //       wx.showToast({
+        //         title: '登录成功',
+        //         icon: 'success'
+        //       })
+        //     } else {
+        //       wx.showToast({
+        //         title: '登录失败,请重试',
+        //         icon: 'error'
+        //       })
+        //     }
+        //   },
+        //   fail: function (error) {
+        //     console.log("login fail")
+        //     wx.showToast({
+        //       title: '登录失败,请重试',
+        //       icon: 'error'
+        //     })
+        //   }
+        // })
+      },
+      fail: (res) => {
+        console.log(' wx.getUserProfile fail', res)
+        wx.showToast({
+          title: '登录失败,请重试',
+          icon: 'error'
+        })
+      },
+      complete: () => {
+        wx.hideLoading()
+      },
+    })
+  }
+}
+
+const saveUserInfo = (userInfo) => {
+  // 设置本地存储(下次登录如果本次存储存在则无需再次登录)
+  wx.setStorageSync('userInfo', userInfo);
+}
+
+const loginOut = () => {
+  wx.setStorageSync('userInfo', null);
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  userLogin: userLogin,
 }
